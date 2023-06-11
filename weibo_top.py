03492b76d025f3c0a1002cb829dff4ef62914fd5
waykom <waykom1@gmail.com>
2023-06-11 16:50:35 +0000

    docker-job

--- weibo_top.py
+++ weibo_top.py
@@ -5,60 +5,54 @@ from datetime import datetime
 
 url = 'https://s.weibo.com/top/summary'
 headers = {
-  'Cookie':
-  'SINAGLOBAL=9984350838163.459.1639458131480; SCF=AhYJZS5_n-2dh-fzeiiEVulWxpdZqWntw9i3SlVXNoxUIwSAX-FUf5SDiBpHtfE-fZIQiT60J8BRFNKo9qXG5VY.; UOR=github.com,s.weibo.com,github.com; _s_tentry=-; Apache=2340719057363.756.1686489602759; ULV=1686489602775:60:1:1:2340719057363.756.1686489602759:1682592070172; SUB=_2A25Jgbp1DeRhGeBL6FMR-CrEyDiIHXVq9qy9rDV8PUNbmtANLVbSkW9NRzPQ6IXxzIoCo2ERDJo3MZKTEEUHcYX6; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WWO7Xc2TqZUpUycnXX6JX6G5JpX5KzhUgL.Foqfe0271hBRe0B2dJLoI05LxK.L1KnLB.qLxKnLBK-LB.qLxK-L1K5LBKMLxKqL12zL1h.LxKqL12zL1hLaUJYt; ALF=1718025636; SSOLoginState=1686489637',
-  'User-Agent':
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+    'Cookie': 'SINAGLOBAL=9984350838163.459.1639458131480; SCF=AhYJZS5_n-2dh-fzeiiEVulWxpdZqWntw9i3SlVXNoxUIwSAX-FUf5SDiBpHtfE-fZIQiT60J8BRFNKo9qXG5VY.; UOR=github.com,s.weibo.com,github.com; _s_tentry=-; Apache=2340719057363.756.1686489602759; ULV=1686489602775:60:1:1:2340719057363.756.1686489602759:1682592070172; SUB=_2A25Jgbp1DeRhGeBL6FMR-CrEyDiIHXVq9qy9rDV8PUNbmtANLVbSkW9NRzPQ6IXxzIoCo2ERDJo3MZKTEEUHcYX6; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WWO7Xc2TqZUpUycnXX6JX6G5JpX5KzhUgL.Foqfe0271hBRe0B2dJLoI05LxK.L1KnLB.qLxKnLBK-LB.qLxK-L1K5LBKMLxKqL12zL1h.LxKqL12zL1hLaUJYt; ALF=1718025636; SSOLoginState=1686489637',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
 }
 response = requests.get(url, headers=headers).text
-href = re.findall(
-  r'<a href="(/weibo\?q=.*&Refer=top)" target="_blank">(.*?)</a>[\s]*<span> (.*?)</span>',
-  response)
+href = re.findall(r'<a href="(/weibo\?q=.*&Refer=top)" target="_blank">(.*?)</a>[\s]*<span> (.*?)</span>', response)
 # 时间格式，用于命名
 date = datetime.now().strftime("%Y-%m-%d")
 time = datetime.now().strftime("%x-%X")
 # 储存原始数据
 for item in href:
-  baseurl, title, index = item
-  url = 'https://s.weibo.com/' + baseurl
-  with open('../weibo_data/' + date + '-origin.md', 'a') as f:
-    f.write('[' + title + ']' + '(' + url + ')' + ' ' + index + "\r\n")
+    baseurl, title, index = item
+    url = 'https://s.weibo.com/'+baseurl
+    with open('../weibo_data/'+date+'-origin.md', 'a')as f:
+        f.write('['+title+']'+'('+url+')'+' '+index+"\r\n")
 # 从本地读取原始数据并去掉空元素
-with open('../weibo_data/' + date + '-origin.md', 'r') as f:
-  datalist = f.read().split('\n')
+with open('../weibo_data/'+date+'-origin.md', 'r')as f:
+    datalist = f.read().split('\n')
 if '' in datalist:
-  while '' in datalist:
-    datalist.remove('')
+    while '' in datalist:
+        datalist.remove('')
 # 新建字典暂存
 counts = {}
 for k in range(len(datalist)):
-  t = datalist[k].split(']')[0].split('[')[1]  # 标题
-  d = datalist[k].split(' ')[-1]  # 热度
-  u = datalist[k].split('](')[1].split(')')[0]  # 链接
-  # 以热度为键
-  counts[int(d)] = u + ', ' + t
+    t = datalist[k].split(']')[0].split('[')[1]   # 标题
+    d = datalist[k].split(' ')[-1]  # 热度
+    u = datalist[k].split('](')[1].split(')')[0]  # 链接
+    # 以热度为键
+    counts[int(d)] = u+', '+t
 # 热度降序
 items = list(counts.items())
 items.sort(key=lambda x: int(x[0]), reverse=True)
 # 获取相同标题的键
 repeatindex = []
 for i in range(len(items)):
-  for k in range(i + 1, len(items)):
-    if items[i][1].split(', ')[-1] == items[k][1].split(', ')[-1]:
-      repeatindex.append(items[k][0])
+    for k in range(i+1, len(items)):
+        if items[i][1].split(', ')[-1] == items[k][1].split(', ')[-1]:
+            repeatindex.append(items[k][0])
 # 去重
 for i in set(repeatindex):
-  counts.pop(i)
+    counts.pop(i)
 # 储存整理好的数据，按热度降序
-if os.path.exists('../weibo_top/result/' + date + '.md'):
-  os.remove('../weibo_top/result/' + date + '.md')
+if os.path.exists('../weibo_top/result/'+date+'.md'):
+    os.remove('../weibo_top/result/'+date+'.md')
 index = 0
 for i in sorted(counts, reverse=True):
-  index += 1
-  with open('../weibo_top/result/' + date + '.md', 'a') as f:
-    f.write(
-      str(index) + '. ' + '[' + counts[i].split(', ')[1] + ']' + '(' +
-      counts[i].split(', ')[0] + ')' + ' ' + str(i) + "\r\n")
+    index += 1
+    with open('../weibo_top/result/'+date+'.md', 'a')as f:
+        f.write(str(index)+'. '+'['+counts[i].split(', ')[1]+']'+'('+counts[i].split(', ')[0]+')'+' '+str(i)+"\r\n")
 item_start = """
 # 今日热门搜索  
 """
@@ -66,11 +60,11 @@ item_end = """
 # 声明  
 本项目的所有数据来源均来自 [新浪微博热搜榜](https://s.weibo.com/top/summary)  
 """
-with open('../weibo_top/result/' + date + '.md', 'r+') as f:
-  content = f.read()
+with open('../weibo_top/result/'+date+'.md', 'r+')as f:
+    content = f.read()
 if os.path.exists('../weibo_top/README.md'):
-  os.remove('../weibo_top/README.md')
-with open('../weibo_top/README.md', 'a') as f:
-  f.write(item_start + content + item_end)
+    os.remove('../weibo_top/README.md')
+with open('../weibo_top/README.md', 'a')as f:
+    f.write(item_start+content+item_end)
 print("Completed")
 print(time)
